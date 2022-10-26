cf466e6549ad4f4c0543fc97bdcea2884e401d1d
Andrew <mightloaf@gmail.com>
2022-10-26 09:14:06 -0400

    Added comments

--- daysCalc.java
+++ daysCalc.java
@@ -1,15 +1,16 @@
 import java.io.IOException;
-import java.sql.Date;
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
 import java.util.Scanner;
 
 public class daysCalc {
+    /*
+     * Checks if the date is valid
+     * i.e. 05/54/2021 is not valid
+     */
     public static boolean isValidDate(String Date) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/uuuu");
         dateFormat.setLenient(false);
@@ -21,6 +22,10 @@ public class daysCalc {
         return true;
     }
 
+    /*
+     * Checks if shortened dates are valid
+     * i.e. 09/70 is not valid
+     */
     public static boolean isValidShortDate(String Date) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd");
         dateFormat.setLenient(false);
@@ -32,15 +37,21 @@ public class daysCalc {
         return true;
     }
 
+    /*
+     * Shortened dates will have a set year depending on:
+     * if date is already passed today's date
+     * if it is passed this method takes today's year and adds it to the end of the
+     * String date
+     */
     public static String returnYear(String date, LocalDate today) throws ParseException {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/uuuu");
-        LocalDate selectDate = LocalDate.parse(date+"/"+today.getYear(), formatter);
+        LocalDate selectDate = LocalDate.parse(date + "/" + today.getYear(), formatter);
 
-        if(selectDate.getMonthValue()>=today.getMonthValue()&&
-        selectDate.getDayOfMonth()>=today.getDayOfMonth()){
-            return date+"/"+today.getYear();
-        }else{
-            return date+"/"+(today.getYear()+1);
+        if (selectDate.getMonthValue() >= today.getMonthValue() &&
+                selectDate.getDayOfMonth() >= today.getDayOfMonth()) {
+            return date + "/" + today.getYear();
+        } else {
+            return date + "/" + (today.getYear() + 1);
         }
     }
 
@@ -63,11 +74,10 @@ public class daysCalc {
         while (input != "Q") {
             System.out.println("Do you want to enter a custom date? (Y/N) or (Q to quit)");
             input = keyboard.nextLine();
-            if (input.equals("Y") || input.equals("y")) {
+            if (input.equals("Y") || input.equals("y")) { // Checks if user answered yes
                 System.out.println("Enter start date (MM/DD/YYYY): ");
                 String first = keyboard.nextLine();
-                if (isValidDate(first)) {
-
+                if (isValidDate(first)) { // If statement determines if date is valid
                 } else if (isValidShortDate(first)) {
                     first = returnYear(first, today);
                 } else {
@@ -76,8 +86,7 @@ public class daysCalc {
                 }
                 System.out.println("Enter end date (MM/DD/YYYY): ");
                 String sec = keyboard.nextLine();
-                if (isValidDate(sec)) {
-
+                if (isValidDate(sec)) { // If statement determines if date is valid
                 } else if (isValidShortDate(sec)) {
                     sec = returnYear(sec, today);
                 } else {
@@ -89,11 +98,11 @@ public class daysCalc {
 
                 long daysBetween = ChronoUnit.DAYS.between(date1, date2);
                 System.out.println("There are " + daysBetween + " days between " + date1 + " and " + date2);
-            } else if (input.equals("N") || input.equals("n")) {
+            } else if (input.equals("N") || input.equals("n")) { // checks if user answered no
                 System.out.println("Quiz, Final or Lab?(Q/F/L): ");
                 input = keyboard.nextLine();
                 if (input.equals("Q") || input.equals("q")) {
-                    System.out.println("Enter Quiz Number(1/2/3/4/5): ");
+                    System.out.println("Enter Quiz Number(1/2/3/4/5): "); // prompts the user to select which quiz
                     input = keyboard.nextLine();
                     if (input.equals("1")) {
                         long daysBetween = ChronoUnit.DAYS.between(today, quiz1);
@@ -114,7 +123,7 @@ public class daysCalc {
                         System.out.println("Error.");
                     }
                 } else if (input.equals("L") || input.equals("l")) {
-                    System.out.println("Enter Lab Number(1/2/3/4): ");
+                    System.out.println("Enter Lab Number(1/2/3/4): "); // prompts the user to select which lab
                     input = keyboard.nextLine();
                     if (input.equals("1")) {
                         long daysBetween = ChronoUnit.DAYS.between(today, lab1);
@@ -131,13 +140,13 @@ public class daysCalc {
                     } else {
                         System.out.println("Error.");
                     }
-                } else if (input.equals("F") || input.equals("f")) {
+                } else if (input.equals("F") || input.equals("f")) { // This is reached if user wants to know final date
                     long daysBetween = ChronoUnit.DAYS.between(today, finalExam);
                     System.out.println("The final exam is " + daysBetween + " days from today.");
                 } else {
                     System.out.println("Error.");
                 }
-            } else if (input.equals("Q") || input.equals("q")) {
+            } else if (input.equals("Q") || input.equals("q")) { // User quits
                 break;
             } else {
                 System.out.println("Error.");
