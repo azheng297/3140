6433f667a7d1e5fc6fd4d114eedeeb242e275bb3
Andrew <mightloaf@gmail.com>
2022-10-26 05:30:52 -0400

    Changed code to fit a while loop and letting users quit (by pressing q)

--- daysCalc.java
+++ daysCalc.java
@@ -19,69 +19,75 @@ public class daysCalc {
                 lab4 = LocalDate.of(2022, 12, 7),
                 finalExam = LocalDate.of(2022, 12, 19);
         Scanner keyboard = new Scanner(System.in);
+        String input = "";
 
-        System.out.println("Do you want to enter a custom date? (Y/N)");
-        String input = keyboard.nextLine();
-        if (input.equals("Y") || input.equals("y")) {
-            System.out.println("Enter start date (MM/DD/YYYY): ");
-            String first = keyboard.nextLine();
-            System.out.println("Enter end date (MM/DD/YYYY): ");
-            String sec = keyboard.nextLine();
-
-            LocalDate date1 = LocalDate.parse(first, formatter), date2 = LocalDate.parse(sec, formatter);
-
-            long daysBetween = ChronoUnit.DAYS.between(date1, date2);
-            System.out.println("There are " + daysBetween + " days between " + date1 + " and " + date2);
-        } else if (input.equals("N") || input.equals("n")) {
-            System.out.println("Quiz, Final or Lab?(Q/F/L): ");
+        while (input != "Q") {
+            System.out.println("Do you want to enter a custom date? (Y/N) or (Q to quit)");
             input = keyboard.nextLine();
-            if (input.equals("Q") || input.equals("q")) {
-                System.out.println("Enter Quiz Number(1/2/3/4/5): ");
+            if (input.equals("Y") || input.equals("y")) {
+                System.out.println("Enter start date (MM/DD/YYYY): ");
+                String first = keyboard.nextLine();
+                System.out.println("Enter end date (MM/DD/YYYY): ");
+                String sec = keyboard.nextLine();
+
+                LocalDate date1 = LocalDate.parse(first, formatter), date2 = LocalDate.parse(sec, formatter);
+
+                long daysBetween = ChronoUnit.DAYS.between(date1, date2);
+                System.out.println("There are " + daysBetween + " days between " + date1 + " and " + date2);
+            } else if (input.equals("N") || input.equals("n")) {
+                System.out.println("Quiz, Final or Lab?(Q/F/L): ");
                 input = keyboard.nextLine();
-                if (input.equals("1")) {
-                    long daysBetween = ChronoUnit.DAYS.between(today, quiz1);
-                    System.out.println("Quiz " + input + " is " + daysBetween + " days from today.");
-                } else if (input.equals("2")) {
-                    long daysBetween = ChronoUnit.DAYS.between(today, quiz2);
-                    System.out.println("Quiz " + input + " is " + daysBetween + " days from today.");
-                } else if (input.equals("3")) {
-                    long daysBetween = ChronoUnit.DAYS.between(today, quiz3);
-                    System.out.println("Quiz " + input + " is " + daysBetween + " days from today.");
-                } else if (input.equals("4")) {
-                    long daysBetween = ChronoUnit.DAYS.between(today, quiz4);
-                    System.out.println("Quiz " + input + " is " + daysBetween + " days from today.");
-                } else if (input.equals("5")) {
-                    long daysBetween = ChronoUnit.DAYS.between(today, quiz5);
-                    System.out.println("Quiz " + input + " is " + daysBetween + " days from today.");
+                if (input.equals("Q") || input.equals("q")) {
+                    System.out.println("Enter Quiz Number(1/2/3/4/5): ");
+                    input = keyboard.nextLine();
+                    if (input.equals("1")) {
+                        long daysBetween = ChronoUnit.DAYS.between(today, quiz1);
+                        System.out.println("Quiz " + input + " is " + daysBetween + " days from today.");
+                    } else if (input.equals("2")) {
+                        long daysBetween = ChronoUnit.DAYS.between(today, quiz2);
+                        System.out.println("Quiz " + input + " is " + daysBetween + " days from today.");
+                    } else if (input.equals("3")) {
+                        long daysBetween = ChronoUnit.DAYS.between(today, quiz3);
+                        System.out.println("Quiz " + input + " is " + daysBetween + " days from today.");
+                    } else if (input.equals("4")) {
+                        long daysBetween = ChronoUnit.DAYS.between(today, quiz4);
+                        System.out.println("Quiz " + input + " is " + daysBetween + " days from today.");
+                    } else if (input.equals("5")) {
+                        long daysBetween = ChronoUnit.DAYS.between(today, quiz5);
+                        System.out.println("Quiz " + input + " is " + daysBetween + " days from today.");
+                    } else {
+                        System.out.println("Error.");
+                    }
+                } else if (input.equals("L") || input.equals("l")) {
+                    System.out.println("Enter Lab Number(1/2/3/4): ");
+                    input = keyboard.nextLine();
+                    if (input.equals("1")) {
+                        long daysBetween = ChronoUnit.DAYS.between(today, lab1);
+                        System.out.println("Lab " + input + " is due " + daysBetween + " days from today.");
+                    } else if (input.equals("2")) {
+                        long daysBetween = ChronoUnit.DAYS.between(today, lab2);
+                        System.out.println("Lab " + input + " is due " + daysBetween + " days from today.");
+                    } else if (input.equals("3")) {
+                        long daysBetween = ChronoUnit.DAYS.between(today, lab3);
+                        System.out.println("Lab " + input + " is due " + daysBetween + " days from today.");
+                    } else if (input.equals("4")) {
+                        long daysBetween = ChronoUnit.DAYS.between(today, lab4);
+                        System.out.println("Lab " + input + " is due " + daysBetween + " days from today.");
+                    } else {
+                        System.out.println("Error.");
+                    }
+                } else if (input.equals("F") || input.equals("f")) {
+                    long daysBetween = ChronoUnit.DAYS.between(today, finalExam);
+                    System.out.println("The final exam is " + daysBetween + " days from today.");
                 } else {
                     System.out.println("Error.");
                 }
-            } else if (input.equals("L") || input.equals("l")) {
-                System.out.println("Enter Lab Number(1/2/3/4): ");
-                input = keyboard.nextLine();
-                if (input.equals("1")) {
-                    long daysBetween = ChronoUnit.DAYS.between(today, lab1);
-                    System.out.println("Lab " + input + " is due " + daysBetween + " days from today.");
-                } else if (input.equals("2")) {
-                    long daysBetween = ChronoUnit.DAYS.between(today, lab2);
-                    System.out.println("Lab " + input + " is due " + daysBetween + " days from today.");
-                } else if (input.equals("3")) {
-                    long daysBetween = ChronoUnit.DAYS.between(today, lab3);
-                    System.out.println("Lab " + input + " is due " + daysBetween + " days from today.");
-                } else if (input.equals("4")) {
-                    long daysBetween = ChronoUnit.DAYS.between(today, lab4);
-                    System.out.println("Lab " + input + " is due " + daysBetween + " days from today.");
-                }else{
-                    System.out.println("Error.");
-                }
-            } else if (input.equals("F") || input.equals("f")) {
-                long daysBetween = ChronoUnit.DAYS.between(today, finalExam);
-                System.out.println("The final exam is " + daysBetween + " days from today.");
-            }else{
+            }else if(input.equals("Q") || input.equals("q")){
+                break;
+            } else {
                 System.out.println("Error.");
             }
-        } else {
-            System.out.println("Error.");
         }
+        keyboard.close();
     }
 }
